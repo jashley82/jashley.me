ffc663a352650f249ceb3923218f40fb48183f53
josh <ashley.joshua@gmail.com>
2016-02-08 21:49:31 -0800

    add unicode methods to models

--- jashley_me/quiz/models.py
+++ jashley_me/quiz/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.question_text
+
 
 class Answer(models.Model):
     category = models.ForeignKey('Category')
@@ -11,6 +14,12 @@ class Answer(models.Model):
     answer_text = models.TextField()
     source = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.answer_text
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
