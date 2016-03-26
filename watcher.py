333293b6cb769540ce5db82b09751600035a7fbc
josh <ashley.joshua@gmail.com>
2016-03-26 13:06:35 -0700

    call Popen directly to fix bug

--- watcher.py
+++ watcher.py
@@ -5,7 +5,7 @@ import datetime
 import time
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 PATTERNS = [
@@ -13,8 +13,8 @@ PATTERNS = [
         '*.html',
         ]
 IGNORE_PATTERNS = [
-        '*src*',
-        'watcher.py',
+        '*src/*',
+        '*watcher.py',
         ]
 BUILDCOMMANDS = [
         ['docker-compose', 'build'], 
@@ -25,43 +25,33 @@ def get_now():
     return datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
 def build_docker():
-    print >> sys.stderr, "Building docker container at {}".format(get_now())
-    os.chdir(BASEDIR)
+    print "[+] Building docker container @{}".format(get_now())
     NULL = open(os.devnull, 'w')
     for cmd in BUILDCOMMANDS:
-        subprocess.call(cmd, stdout=NULL)
-
-# def run_tests():
-    # print >> sys.stderr, "Running unit tests at %s" % get_now()
-    # os.chdir(BASEDIR)
-    # subprocess.call(r'python -m unittest discover -b')
-
+        subprocess.Popen(cmd, stdout=NULL)
+    return
 
 class ChangeHandler(PatternMatchingEventHandler):
     patterns = PATTERNS
     ignore_patterns = IGNORE_PATTERNS
-
+    
     def on_any_event(self, event):
-        if event.is_directory:
-            return
-        print event.src_path, event.event_type
+        print "[*] {} {}".format(event.src_path, event.event_type)
         build_docker()
-        # run_tests()
-        
 
 def main():
+    os.chdir(BASEDIR)
+    print "[*] Starting watchdog in {} @{}".format(BASEDIR, get_now())
     build_docker()
-    while 1:
-        event_handler = ChangeHandler()
-        observer = Observer()
-        observer.schedule(event_handler, BASEDIR, recursive=True)
-        observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+    observer = Observer()
+    observer.schedule(ChangeHandler(), BASEDIR, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 if __name__ == '__main__':
     main()
