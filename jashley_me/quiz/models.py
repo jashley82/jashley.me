8a8584d24a38ab327f7d7f9edc17f58c64903d39
josh <ashley.joshua@gmail.com>
2016-04-01 19:51:29 -0700

    better faster stronger

--- jashley_me/quiz/models.py
+++ jashley_me/quiz/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     category = models.ForeignKey('Category', default=0)
@@ -10,20 +17,9 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    question = models.OneToOneField(
-            'Question', 
-            on_delete=models.CASCADE, 
-            primary_key=True
-            )
+    question = models.ForeignKey('Question', default=0)
     answer_text = models.TextField()
     source = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.answer_text
-
-
-class Category(models.Model):
-    title = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return self.title
--- jashley_me/quiz/views.py
+++ jashley_me/quiz/views.py
@@ -6,19 +6,19 @@ from quiz.models import Answer, Question
 
 
 def do_quiz(request):
-    answer = Answer.objects.order_by('?').first()
-    if answer: 
-        form = QuizForm({'answer_id': answer.id})
+    question = Question.objects.order_by('?').first()
+    if question: 
+        form = QuizForm({'question_id': question.id})
     else:
         form = QuizForm()
-    return render(request, 'quiz/do_quiz.html', {'answer': answer, 'form': form})
+    return render(request, 'quiz/do_quiz.html', {'question': question, 'form': form})
   
 def check_answer(request):
     if request.method == 'POST':
         form = QuizForm(request.POST)
         if form.is_valid():
-            answer = Answer.objects.get(id = request.POST.get('answer_id'))
-            if request.POST.get('question_text') == answer.question.question_text:
+            question = Question.objects.get(id = request.POST.get('question_id'))
+            if request.POST.get('answer_text') == question.answer_set.first().answer_text:
                 messages.add_message(request, messages.SUCCESS, 'Correct!')
             else:
                 messages.add_message(request, messages.ERROR, 'Wrong!')
