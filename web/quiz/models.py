def9ef8b988bcb66c7fcc4dbf20620ab4bb5ec9a
josh <ashley.joshua@gmail.com>
2016-04-08 21:32:07 -0700

    pretty fucking good

--- web/quiz/models.py
+++ web/quiz/models.py
@@ -18,8 +18,20 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey('Question', default=0)
-    answer_text = models.TextField()
+    answer_text = models.CharField(max_length=255)
     source = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.answer_text
+
+
+class ScoreCard(models.Model):
+    question = models.ForeignKey('Question')
+    answered_text = models.CharField(max_length=255)
+    time_completed = models.CharField(max_length=255)
+    pass_fail = models.BooleanField()
+    user_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def count(self, obj):
+        return ScoreCard.objects.filter(question=obj).count()
